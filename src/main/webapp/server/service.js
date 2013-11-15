7ea794a70a1c22b8d5149e4543e440b7cb410469
Yujie Zhou <oliverzy@hotmail.com>
2013-11-15 18:27:14 +0800

    Use Q.js instead of async.js

--- src/main/webapp/server/service.js
+++ src/main/webapp/server/service.js
@@ -1,29 +1,26 @@
-var _ = require('lodash');
-var async = require('async');
+var Q = require('q');
+Q.longStackSupport = true;
 var mongoose = require('mongoose');
 var AccountEntryType = require('./model').AccountEntryType;
 var Account = mongoose.model('Account');
 var AccountEntry = mongoose.model('AccountEntry');
 var MealRecord = mongoose.model('MealRecord');
 
-exports.splitMealRecord = function spiltMealRecord(mealRecord, cb) {
-  Account.findOne({name: mealRecord.payer}, function (err, payer) {
-
+exports.splitMealRecord = function spiltMealRecord(mealRecord) {
+  return Account.findOne({name: mealRecord.payer}).exec()
+  .then(function (payer) {
     payer.debit(mealRecord.date, mealRecord.amount);
-    payer.save(function (err) {
-
-      var avgCost = mealRecord.amount / mealRecord.participants.length;
-      Account.find().in('name', mealRecord.participants).exec(function (err, participants) {
-        
-        async.each(participants, function (participant, done) {
-          participant.credit(mealRecord.date, avgCost);
-          participant.save(function (err) { 
-            done(); 
-          });
-        }, function (err) {
-          cb(err);
-        });
-      });
+    return Q.ninvoke(payer, 'save');
+  })
+  .then(function() {
+    return Account.find().in('name', mealRecord.participants).exec();
+  })
+  .then(function(participants) {
+    var avgCost = mealRecord.amount / mealRecord.participants.length;
+    var results = participants.map(function (participant) {
+      participant.credit(mealRecord.date, avgCost);
+      return Q.ninvoke(participant, 'save');
     });
+    return Q.all(results);
   });
 };
--- src/main/webapp/server/test.js
+++ src/main/webapp/server/test.js
@@ -1,4 +1,6 @@
 var async = require('async');
+var Q = require('q');
+Q.longStackSupport = true;
 var mongoose = require('mongoose');
 var services = require('./service.js');
 mongoose.connect('mongodb://localhost/fantuan');
@@ -10,38 +12,60 @@ db.once('open', function() {
   var AccountEntry = mongoose.model('AccountEntry');
   var MealRecord = mongoose.model('MealRecord');
   
-  async.series([
-    function(done) {
-      mongoose.connection.collections[Account.collection.name].drop(function() {
-        mongoose.connection.collections[MealRecord.collection.name].drop(function() {
-          done();
-        });
-      });
-    },
-    function(done) {
-      var account = new Account({name: "Oliver Zhou", password: "123", enabled: true, balance: 0, entries: []});
-      //account.debit(Date.now(), 200, "Fuck");
-      account.save(function (err, account) {
-        var account2 = new Account({name: "Gary Chang", password: "123", enabled: true, balance: 0, entries: []});
-        account2.save(function (err, account2) {
-          var record = new MealRecord({amount: 200, restaurant: "汉堡王", date: Date.now(), payer: account.name, participants: [account.name, account2.name]});
-          record.save(function (err, record) {
-            console.log(record);
+  function cleanup() {
+    return Q.all([Q.ninvoke(mongoose.connection.collections[Account.collection.name], 'drop'),
+      Q.ninvoke(mongoose.connection.collections[MealRecord.collection.name], 'drop')]);
+  }
 
-            console.log(services);
-            services.splitMealRecord(record, function() {
-              console.log("Fuck");
-              done();
-            });
-          });
-        });
-      });
-    },
-    function(done) {
-      mongoose.disconnect(function() {
-        done();
-      });
-    }
-  ]);
+  function insertData() {
+    var account = new Account({name: "Oliver Zhou", password: "123", enabled: true, balance: 0, entries: []});
+    var account2 = new Account({name: "Gary Chang", password: "123", enabled: true, balance: 0, entries: []});
+    return Q.all([Q.ninvoke(account, 'save'), Q.ninvoke(account2, 'save')]).then(function () {
+      var record = new MealRecord({amount: 200, restaurant: "汉堡王", date: Date.now(), payer: account.name, 
+        participants: [account.name, account2.name]});
+      return Q.ninvoke(record, 'save');
+    }).then(function(record) {
+      console.log(record);
+      return services.splitMealRecord(record[0]);
+    });
+  }
+
+  function disconnect() {
+    return Q.ninvoke(mongoose, 'disconnect');
+  }
+
+  cleanup().then(insertData).then(disconnect).catch(console.log);
+
+  // async.series([
+  //   function(done) {
+  //     mongoose.connection.collections[Account.collection.name].drop(function() {
+  //       mongoose.connection.collections[MealRecord.collection.name].drop(function() {
+  //         done();
+  //       });
+  //     });
+  //   },
+  //   function(done) {
+  //     account.save(function (err, account) {
+  //       var account2 = new Account({name: "Gary Chang", password: "123", enabled: true, balance: 0, entries: []});
+  //       account2.save(function (err, account2) {
+  //         var record = new MealRecord({amount: 200, restaurant: "汉堡王", date: Date.now(), payer: account.name, participants: [account.name, account2.name]});
+  //         record.save(function (err, record) {
+  //           console.log(record);
+
+  //           console.log(services);
+  //           services.splitMealRecord(record, function() {
+  //             console.log("Fuck");
+  //             done();
+  //           });
+  //         });
+  //       });
+  //     });
+  //   },
+  //   function(done) {
+  //     mongoose.disconnect(function() {
+  //       done();
+  //     });
+  //   }
+  // ]);
 
 });
